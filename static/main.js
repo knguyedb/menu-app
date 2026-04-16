e1fcdf6f8bcc2bdde10d52c5cb5cbf9656340705
Khoi Nguyen <111422797+knguyedb@users.noreply.github.com>
2026-04-16 13:32:56 +0700

    Update main.js

--- static/main.js
+++ static/main.js
@@ -30,7 +30,7 @@ form.addEventListener('submit', async (e) => {
   hideResult();
 
   try {
-    const res = await fetch('http://localhost:5000/api/generate_recipe', {
+    const res = await fetch('https://menu-app-4pqp.onrender.com', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ ingredients, diet, allergies }),
